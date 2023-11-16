11c5bac5ede03f9ac4a234125567f0e426a0eea6
lakst <luk.lakstinis@kvkvedu.lt>
2023-11-17 00:53:05 +0200

    Profile info design and header done

---
+++ kvk-internship-scheduler-frontend/src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+    
+    <v-card style="position:static">
+      <v-layout >
+        <v-navigation-drawer
+          v-model="drawer"
+          temporary
+        >
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+            title="John Leider"
+          ></v-list-item>
+  
+          <v-divider></v-divider>
+  
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+            <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+  
+   
+            <v-btn
+              color="primary"
+              @click.stop="drawer = !drawer"
+              style="position: absolute;  height:12%; width:100px;"
+            >
+            <v-icon  icon="mdi-menu" size="x-large"></v-icon>
+            </v-btn>
+      </v-layout>
+      <Header style="width: 100%;">
+    </Header>
+    </v-card>
+    
+  </template>
+  
+  <script>
+    import Header from "@/components/HeaderForMobile.vue";
+    export default {
+        components:{
+            Header,
+        },
+      data () {
+        return {
+          drawer: null,
+        }
+      },
+    }
+  </script>
+
+  <style>
+
+</style>
--- kvk-internship-scheduler-frontend/src/plugins/vuetify.js
+++ kvk-internship-scheduler-frontend/src/plugins/vuetify.js
@@ -11,6 +11,7 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
@@ -23,4 +24,5 @@ export default createVuetify({
       },
     },
   },
+
 })

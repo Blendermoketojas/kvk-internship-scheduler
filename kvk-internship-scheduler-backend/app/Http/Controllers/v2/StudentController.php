808c06647ec8dbe61ab935fcfd6b54aaef96c664
Tadas Andrijauskas <tadasandrijauskass@gmail.com>
2023-11-19 18:37:36 +0200

    - search groups - search students

--- kvk-internship-scheduler-backend/app/Http/Controllers/v2/StudentController.php
+++ kvk-internship-scheduler-backend/app/Http/Controllers/v2/StudentController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers\v2;
 use App\Http\Controllers\Controller;
 use App\Models\StudentGroup;
 use App\Models\UserProfile;
-use http\Env\Request;
+use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function searchStudentGroups(Request $request) {
-        return StudentGroup::where('group_identifier', $request->input('groupIdentifier'));
+    public function searchStudentGroups(Request $request)
+    {
+        $groupIdentifier = $request->input('groupIdentifier');
+        $studentGroups = StudentGroup::whereRaw('LOWER(group_identifier) LIKE ?', ['%' . strtolower($groupIdentifier) . '%'])->get();
+
+        return response()->json($studentGroups);
     }
 
-    public function searchStudents() {
-        return UserProfile::where('role_id', 5)->where('');
+    public function searchStudents(Request $request)
+    {
+        $fullName = $request->input('fullName');
+
+        $query = UserProfile::where('role_id', 5)
+                ->whereRaw('LOWER(fullname) LIKE ?', ['%' . strtolower($fullName) . '%'])
+            ->get();
+        return response()->json($query);
     }
+
 }

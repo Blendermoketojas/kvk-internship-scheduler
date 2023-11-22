a8efccfd128fb28b64281a01863f8e14ad560245
Tadas Andrijauskas <tadasandrijauskass@gmail.com>
2023-11-22 17:39:04 +0200

    - REFACTORING: user profile picture update logic moved from controller to service - update profile picture is fixed hopefully

--- kvk-internship-scheduler-backend/app/Http/Controllers/v2/UserProfileController.php
+++ kvk-internship-scheduler-backend/app/Http/Controllers/v2/UserProfileController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\v2;
 
 use App\Http\Controllers\Controller;
+use App\Services\ManageUserProfile\UpdateUserProfilePictureService;
+use App\Services\ManageUserProfile\UpdateUserProfileService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserProfileController extends Controller
 {
@@ -14,60 +18,19 @@ class UserProfileController extends Controller
         return response()->json($userProfile);
     }
 
-    public function updateProfilePicture(Request $request) {
-        $request->validate(['image' => 'required|image|max:2048']);
-
-        $userProfile = auth()->user()->userProfile;
-        $path = $request->file('image')->store('profile_pictures', 'public');
-
-        $userProfile->image_path = $path;
-        $userProfile->save();
-
-        return response()->json(['message' => 'Profile picture updated successfully.']);
-        $userProfile = auth()->user()->userProfile()->with('user')->get();
-        return response()->json($userProfile[0]);
+    /**
+     * @throws ValidationException
+     */
+    public function updateProfilePicture(Request $request) : JsonResponse
+    {
+        return (new UpdateUserProfilePictureService($request))->execute();
     }
 
-    public function update(Request $request)
+    /**
+     * @throws ValidationException
+     */
+    public function update(Request $request): JsonResponse
     {
-        $request->validate([
-            'password' => 'required|string|min:6',
-        ]);
-
-        $user = auth()->user();
-        $userProfile = $user->userProfile;
-
-        if (!strcmp($userProfile->email, $request->input('email'))) {
-            $request->validate([
-                'email' => 'required|string|email|max:255|unique:users',
-            ]);
-        }
-
-        if (!$userProfile) {
-            return response()->json(['error' => 'User profile does not exist'], 404);
-        }
-
-        $isUpdated = $userProfile->update([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'fullname' => $request->input('first_name') . ' ' . $request->input('last_name'),
-            'description' => $request->input('description'),
-            'company_id' => $request->input('company_id'),
-            'country' => $request->input('country'),
-            'address' => $request->input('address'),
-            'image_path' => $request->input('img_path'),
-        ]);
-
-        if ($isUpdated) {
-            if (!Hash::check($request->input('password'), auth()->user()->getAuthPassword())) {
-                $user->update([
-                    'password' => Hash::make($request->input('password')),
-                    'email' => $request->input('email')
-                ]);
-            }
-        }
-
-        $userProfile['user'] = ['email' => $user['email']];
-        return response()->json($userProfile);
+        return (new UpdateUserProfileService($request))->execute();
     }
 }

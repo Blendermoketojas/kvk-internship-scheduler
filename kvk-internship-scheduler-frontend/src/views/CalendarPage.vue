d48984bf328f8fa1193c20803c995afaf6bea562
lakst <luk.lakstinis@kvkvedu.lt>
2023-11-28 22:06:10 +0200

    comment creation done

--- kvk-internship-scheduler-frontend/src/views/CalendarPage.vue
+++ kvk-internship-scheduler-frontend/src/views/CalendarPage.vue
@@ -1,14 +1,21 @@
 <template>
   <header-nav />
-  <DxScheduler id="scheduler" :adaptivity-enabled="true" :views="views" current-view="week" :data-source="dataSource"
-    @appointmentAdded="showValue">
+  <DxScheduler
+    id="scheduler"
+    :adaptivity-enabled="true"
+    :views="views"
+    current-view="week"
+    :data-source="dataSource"
+    @appointmentAdded="showValue"
+  >
   </DxScheduler>
 </template>
 <script>
 import "devextreme/dist/css/dx.light.css";
 import { DxScheduler } from "devextreme-vue/scheduler";
 import HeaderNav from "@/components/DesktopHeader.vue";
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import apiClient from "@/utils/api-client";
 
 export default {
   components: {
@@ -20,34 +27,52 @@ export default {
     return {
       views: ["month", "week"],
       dataSource: [],
-
-
+      internship_id: null,
     };
   },
   methods: {
     showValue(e) {
       const appointmentData = e.appointmentData;
-      console.log(e);
-      console.log(appointmentData);
 
+      const formattedStartDate = this.formatDate(appointmentData.startDate);
+      const formattedEndDate = this.formatDate(appointmentData.endDate);
+
+      const dataToSend = {
+        internshipId: this.internship_id,
+        comment: appointmentData.description,
+        dateFrom: formattedStartDate,
+        dateTo: formattedEndDate,
+      };
+
+      apiClient
+        .post("/comments", dataToSend)
+    },
+
+    formatDate(date) {
+      const offset = date.getTimezoneOffset();
+      const adjustedDate = new Date(date.getTime() - offset * 60 * 1000);
+      return adjustedDate.toISOString().split("T")[0];
     },
     updateCurrentInternship(internship) {
-      this.$store.commit('setCurrentInternship', internship)
+      this.$store.commit("setCurrentInternship", internship);
     },
     getActiveInternship() {
-      // partraukimo logika this.updateCurrentIntership(response.data)
-    }
+      apiClient
+        .get("/internship-active", { withCredentials: true })
+        .then((response) => {
+          this.internship_id = response.data.id;
+          console.log("Active internship ID:", this.internship_id);
+        });
+    },
   },
   computed: {
-    ...mapGetters([
-      'getCurrentInternship'
-    ])
+    ...mapGetters(["getCurrentInternship"]),
   },
   mounted() {
     if (!this.getCurrentInternship) {
       this.getActiveInternship();
     }
-  }
+  },
 };
 </script>
 <style scoped>

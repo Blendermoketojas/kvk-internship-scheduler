be47e68954b085bc57997196161ce7e02b397718
Tadas Andrijauskas <tadasandrijauskass@gmail.com>
2023-11-28 19:37:36 +0200

    - merge conflicts fix

--- kvk-internship-scheduler-frontend/src/views/CalendarPage.vue
+++ kvk-internship-scheduler-frontend/src/views/CalendarPage.vue
@@ -1,21 +1,14 @@
 <template>
   <header-nav />
-  <DxScheduler
-    id="scheduler"
-    :adaptivity-enabled="true"
-    :views="views"
-    current-view="week"
-    :data-source="dataSource"
-    @appointmentAdded="showValue"
-  >
+  <DxScheduler id="scheduler" :adaptivity-enabled="true" :views="views" current-view="week" :data-source="dataSource"
+    @appointmentAdded="showValue">
   </DxScheduler>
-
 </template>
 <script>
 import "devextreme/dist/css/dx.light.css";
 import { DxScheduler } from "devextreme-vue/scheduler";
 import HeaderNav from "@/components/DesktopHeader.vue";
-  import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
@@ -26,9 +19,9 @@ export default {
   data() {
     return {
       views: ["month", "week"],
-     dataSource:[],
- 
-   
+      dataSource: [],
+
+
     };
   },
   methods: {
@@ -36,31 +29,27 @@ export default {
       const appointmentData = e.appointmentData;
       console.log(e);
       console.log(appointmentData);
-  
-    }
-      updateCurrentInternship(internship) {
-        this.$store.commit('setCurrentInternship', internship)
-      },
-      getActiveInternship() {
-        // partraukimo logika this.updateCurrentIntership(response.data)
-      }
+
     },
+    updateCurrentInternship(internship) {
+      this.$store.commit('setCurrentInternship', internship)
+    },
+    getActiveInternship() {
+      // partraukimo logika this.updateCurrentIntership(response.data)
+    }
   },
+  computed: {
+    ...mapGetters([
+      'getCurrentInternship'
+    ])
+  },
+  mounted() {
+    if (!this.getCurrentInternship) {
+      this.getActiveInternship();
+    }
+  }
 };
 </script>
-    },
-    computed: {
-      ...mapGetters([
-        'getCurrentInternship'
-      ])
-    },
-    mounted() {
-      if (!this.getCurrentInternship) {
-        this.getActiveInternship();
-      }
-    }
-  };
-  </script>
 <style scoped>
 #scheduler {
   height: 600px;

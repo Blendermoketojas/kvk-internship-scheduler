038aa60ed90f8ff5f316c50fef1aa653a09a85c9
Tadas Andrijauskas <tadasandrijauskass@gmail.com>
2023-11-30 00:14:18 +0200

    - documents page template

--- kvk-internship-scheduler-frontend/src/components/documents/DocumentContainer.vue
+++ kvk-internship-scheduler-frontend/src/components/documents/DocumentContainer.vue
@@ -1,19 +1,58 @@
 <template>
-    <v-expansion-panels>
-        <v-expansion-panel title="Title"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima">
-        </v-expansion-panel>
-    </v-expansion-panels>
+    <div class="document-container">
+        <div class="d-flex align-items-center document-container-title">
+            <v-icon icon="mdi-chevron-down" size="70"></v-icon>
+            <div class="text-container">
+                <span class="fs-2">{{ containerName }}</span>
+            </div>
+        </div>
+        <div class="document-vertical-line"></div>
+        <div class="d-flex flex-column">
+            <document-row v-for="i in 5"></document-row>
+        </div>
+    </div>
 </template>
 
 <script>
+import DocumentRow from './DocumentRow.vue';
+
 export default {
-    
+    components: {
+        DocumentRow,
+    },
+    props: {
+        containerName: {
+            required: true,
+            type: String,
+            default: 'IT sistemų praktika'
+        }
+    }
 }
 </script>
 
 <style>
-.document-container-body {
-    
+.document-container {
+    border: 2px solid black;
+    border-radius: 5px;
 }
+
+.document-vertical-line {
+    border-bottom: 2px solid black;
+}
+
+.text-container {
+    display: flex;
+    align-items: center;
+    flex-grow: 1; /* Optional: It helps the text container take up the remaining space */
+}
+
+.document-container-title {
+    font-weight: 700;
+}
+
+.document-container-body {
+
+}
+
+/* Other styles */
 </style>

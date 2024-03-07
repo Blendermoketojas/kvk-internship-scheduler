a0e65b9af43e6e12cc7909562d38244ffa2a9322
lakst <luk.lakstinis@kvkvedu.lt>
2024-03-07 21:40:50 +0200

    responsive design fix and pagination fix

--- kvk-internship-scheduler-frontend/src/views/OwnedInternshipsPage.vue
+++ kvk-internship-scheduler-frontend/src/views/OwnedInternshipsPage.vue
@@ -1,27 +1,23 @@
 <template>
   <custom-header v-if="isDesktop" />
   <mobile-nav v-if="!isDesktop" />
-<div class="mainPage">
-<h1>Priskirti studentai</h1>
-<h2>Čia galite matyti jusų priskirtų studentų sąrašą</h2>
-<div class="contentDiv">
-    
-    <v-data-table-server
-    v-model:items-per-page="itemsPerPage"
-    :headers="headers"
-    :items-length="totalItems"
-    :items="serverItems"
-    :loading="loading"
-    item-value="name"
-    @update:options="loadItems"
-   loading-text="Kraunama informacija"
-   items-per-page-text="Rodomas studentu skaičius"
- 
-  ></v-data-table-server>
-
-</div>
-</div>
-
+  <div class="mainPage">
+    <h1>Priskirti studentai</h1>
+    <h2>Čia galite matyti jusų priskirtų studentų sąrašą</h2>
+    <div class="contentDiv">
+      <v-data-table-server
+        v-model:items-per-page="itemsPerPage"
+        :headers="headers"
+        :items-length="totalItems"
+        :items="serverItems"
+        :loading="loading"
+        item-value="name"
+        @update:options="loadItems"
+        loading-text="Kraunama informacija"
+        items-per-page-text="Rodomas studentu skaičius"
+      ></v-data-table-server>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,49 +25,47 @@ import customHeader from "@/components/DesktopHeader.vue";
 import apiClient from "@/utils/api-client";
 import mobileNav from "@/components/MobileSidebar.vue";
 
-  const FakeAPI = {
-    async fetch ({ page, itemsPerPage }) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          const start = (page - 1) * itemsPerPage
-          const end = start + itemsPerPage
-          const items = desserts.slice()
+const FakeAPI = {
+  async fetch({ page, itemsPerPage }) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const start = (page - 1) * itemsPerPage;
+        const end = start + itemsPerPage;
+        const items = desserts.slice();
 
-          if (sortBy.length) {
-            const sortKey = sortBy[0].key
-            const sortOrder = sortBy[0].order
-            items.sort((a, b) => {
-              const aValue = a[sortKey]
-              const bValue = b[sortKey]
-              return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
-            })
-          }
+        if (sortBy.length) {
+          const sortKey = sortBy[0].key;
+          const sortOrder = sortBy[0].order;
+          items.sort((a, b) => {
+            const aValue = a[sortKey];
+            const bValue = b[sortKey];
+            return sortOrder === "desc" ? bValue - aValue : aValue - bValue;
+          });
+        }
 
-          const paginated = items.slice(start, end)
+        const paginated = items.slice(start, end);
 
-          resolve({ items: paginated, total: items.length })
-        }, 500)
-      })
-    },
-  }
-
+        resolve({ items: paginated, total: items.length });
+      }, 500);
+    });
+  },
+};
 
 export default {
   data() {
     return {
       isDesktop: window.innerWidth > 950,
-        itemsPerPage: 5,
+      itemsPerPage: 5,
       headers: [
-        { title: 'Vardas Pavardė', key: 'fullname', align: 'start' },
-        { title: 'Grupė', key: 'group_identifier', align: 'end' },
-        { title: 'Mokslo rūšis', key: 'field_of_study', align: 'end' },
-     
+        { title: "Vardas Pavardė", key: "fullname", align: "start" },
+        { title: "Grupė", key: "group_identifier", align: "end" },
+        { title: "Mokslo rūšis", key: "field_of_study", align: "end" },
       ],
+      allItems: [],
       serverItems: [],
       loading: true,
       totalItems: 0,
-
-
+      currentPage: 1,
     };
   },
   components: {
@@ -83,34 +77,39 @@ export default {
       this.isDesktop = window.innerWidth > 950;
     },
 
-    loadItems ({ page, itemsPerPage, sortBy }) {
-        this.loading = true
+    loadItems({ page, itemsPerPage, sortBy }) {
+      this.loading = true;
+      this.currentPage = page;
+      this.itemsPerPage = itemsPerPage;
+      const start = (page - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      this.serverItems = this.allItems.slice(start, end);
+      this.totalItems = this.allItems.length;
 
+
+      apiClient
       apiClient.get(`/linked-students?`)
-        .then(response => {
-  
-          this.serverItems = response.data.map(item => ({
-            ...item,
-            group_identifier: item.student_group.group_identifier,
-            field_of_study: item.student_group.field_of_study,
+      .then(response => {
+        this.allItems = response.data.map(item => ({
+          ...item,
+          group_identifier: item.student_group.group_identifier,
+          field_of_study: item.student_group.field_of_study,
         }));
-          this.totalItems = response.data.length;
-        })
-        .catch(error => {
-          console.error("Error fetching data:", error);
-   
+        this.totalItems = this.allItems.length;
+        this.loadItems({ page: this.currentPage, itemsPerPage: this.itemsPerPage });
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
         })
         .finally(() => {
           this.loading = false;
         });
     },
-
   },
   mounted() {
-     this.loadItems({ page: 1, itemsPerPage: this.itemsPerPage, sortBy: [] });
+    this.loadItems({ page: 1, itemsPerPage: this.itemsPerPage, sortBy: [] });
   },
   created() {
-
     window.addEventListener("resize", this.handleResize);
   },
   beforeDestroy() {
@@ -122,18 +121,18 @@ export default {
 <style>
 @import "@/styles/InternshipStyle/ownedStudentList.css";
 
-.mainPage{
-   padding:0 200px; 
+.mainPage {
+  padding: 0 200px;
 }
-  
-  h2 {
-    display: inline-block;
-    font-size: 15px;
-    color: rgb(170, 167, 167);
-    font-weight: 400;
-  }
 
-  .contentDiv{
-padding: 0 200px;
-  }
+h2 {
+  display: inline-block;
+  font-size: 15px;
+  color: rgb(170, 167, 167);
+  font-weight: 400;
+}
+
+.contentDiv {
+  padding: 0 200px;
+}
 </style>
